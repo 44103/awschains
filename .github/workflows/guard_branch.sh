da03541cd352be0d6016ccae279b0986dc537da9
Kenko Yoshitomi <universeengineer@gmail.com>
2023-06-03 10:31:06 +0900

    Replace branch to production for release

--- .github/workflows/guard_branch.sh
+++ .github/workflows/guard_branch.sh
@@ -3,9 +3,9 @@
 BRANCHES=$(git branch --contains ${{ github.ref_name }})
 set -- $BRANCHES
 for BRANCH in $BRANCHES; do
-  if [[ "$BRANCH" == "main" ]]; then
+  if [[ "$BRANCH" == "$1" ]]; then
     exit 0
   fi
 done
-echo "You can release only main branch."
+echo "You can release only $1 branch."
 exit 1
