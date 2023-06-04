849c011a4ed95b4df7a7206883d782cb270e8c29
Kenko Yoshitomi <universeengineer@gmail.com>
2023-06-04 13:43:49 +0900

    Fix command to run guard_branch.sh

--- .github/workflows/guard_branch.sh
+++ .github/workflows/guard_branch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-BRANCHES=$(git branch --contains ${{ github.ref_name }})
+TARGET_BRANCH=$1
+TAG=$2
+BRANCHES=$(git branch --contains $TAG)
 set -- $BRANCHES
 for BRANCH in $BRANCHES; do
-  if [[ "$BRANCH" == "$1" ]]; then
+  if [[ "$BRANCH" == "$TARGET_BRANCH" ]]; then
     exit 0
   fi
 done
-echo "You can release only $1 branch."
+echo "You can release only $TARGET_BRANCH branch."
 exit 1
